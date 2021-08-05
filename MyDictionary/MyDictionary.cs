779d687036980e4e2769b9978fa62c0c10c6f40e
mervenrkrky <burcu90132@gmail.com>
2021-08-05 22:54:58 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _key;
+        TValue[] _value;
+
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+
+        }
+        public void Add(TKey item1,TValue item2)
+        {
+
+            TKey[] _temparraykey = _key;
+            TValue[] _temparrayvalues = _value;
+
+            _key = new TKey[_key.Length + 1];
+            _value = new TValue[_value.Length + 1];
+            for (int i = 0; i < _temparraykey.Length; i++)
+            {
+                _key[i] = _temparraykey[i];
+                _value[i] = _temparrayvalues[i];
+            }
+            _key[_key.Length - 1] = item1;
+            _value[_value.Length - 1] = item2;
+
+            Console.WriteLine(item1+ " numaralı  " + item2 + " adlı kullanıcı sisteme eklendi.");
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary1 = new MyDictionary<int, string>();
+            myDictionary1.Add(1, "Mervenur");
+
+            MyDictionary<int, string> myDictionary2 = new MyDictionary<int, string>();
+            myDictionary2.Add(2, "Ayşenur:)");
+        } 
+    }
+}
